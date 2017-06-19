71a7515560bacb0e7962c75cc2765120343419f3
Евгений Плотников <bad4iz@gmail.com>
2017-06-19 14:31:10 +0400

    state

--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,8 @@
 function updateState(state, action) {
     if (action.type === 'INCREMENT') {
-        return state + action.amount;
+        return { count: state.count + action.amount};
     } else if (action.type === 'DECREMENT') {
-        return state - action.amount;
+        return { count: state.count - action.amount};
     } else {
         return state;
     }
@@ -21,7 +21,7 @@ class Store {
     
     update(action) {
         this._state = this._updateState(this._state, action);
-        this._callbacks.forEach(callback => callback())
+        this._callbacks.forEach(callback => callback());
     }
     
     subscribe(callback) {
@@ -30,7 +30,9 @@ class Store {
     }
 }
 
-const store = new Store(updateState, 0);
+initialState = { count: 0};
+    
+const store = new Store(updateState, initialState);
 
 const incrementAction = { type: 'INCREMENT', amount: 5};
 const decrementAction = { type: 'DECREMENT', amount: 44};
